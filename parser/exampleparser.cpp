3c151daba1a10432bbe9eb786c1bed489f6967d5
Filipe Correia <68955432+fmcorreia04@users.noreply.github.com>
2023-10-12 11:25:46 +0100

    added some example code for parsing and the files csv

---
+++ parser/exampleparser.cpp
@@ -0,0 +1,39 @@
+//
+// Created by Filipe Correia on 12/10/2023.
+//
+
+
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#include <vector>
+
+int main(){
+    std::ifstream classes("classes.csv");
+    std::string line;
+
+    if (!classes.is_open()) {
+        std::cout << "not ok";
+        return 1;
+    }
+    std::vector<std::vector<std::string>> theList;
+
+    while (getline(classes,line))
+    {
+        std::vector<std::string> attributes;
+        std::istringstream iss(line);
+        std::string attribute;
+        while (getline(iss, attribute, ',')) {
+            attributes.push_back(attribute);
+        }
+        theList.push_back(attributes);
+    }
+    classes.close();
+    for (auto &lin : theList) {
+        for(auto &el : lin){
+            std::cout << el;
+        }
+        std::cout << "/n";
+    }
+
+}
